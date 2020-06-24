eb725078f45255e3dfa36525fdadecf9f5ccaa06
Tuấn Nguyễn Hoàng <18521601@ms.uit.edu.vn>
2020-06-24 14:10:51 +0700

    comment

--- Đồ án Caro/Form8.Designer.cs
+++ Đồ án Caro/Form8.Designer.cs
@@ -74,9 +74,12 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(655, 395);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(607, 385);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(133, 23);
+            this.button1.Size = new System.Drawing.Size(168, 43);
             this.button1.TabIndex = 3;
             this.button1.Text = "Tạo phòng";
             this.button1.UseVisualStyleBackColor = true;
